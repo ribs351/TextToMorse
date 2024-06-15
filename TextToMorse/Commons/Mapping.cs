56e51e39d177def81d3caaa701f908d705027a4a
Ribs <ribs351@gmail.com>
2024-06-15 16:10:52 +0700

    Add project files.

---
+++ TextToMorse/Commons/Mapping.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextToMorse.Commons
+{
+    public class Mapping
+    {
+        public static Dictionary<char, string> morseCodeDictionary = new Dictionary<char, string>
+        {
+            { 'A', ".-" }, { 'B', "-..." }, { 'C', "-.-." },
+            { 'D', "-.." }, { 'E', "." }, { 'F', "..-." },
+            { 'G', "--." }, { 'H', "...." }, { 'I', ".." },
+            { 'J', ".---" }, { 'K', "-.-" }, { 'L', ".-.." },
+            { 'M', "--" }, { 'N', "-." }, { 'O', "---" },
+            { 'P', ".--." }, { 'Q', "--.-" }, { 'R', ".-." },
+            { 'S', "..." }, { 'T', "-" }, { 'U', "..-" },
+            { 'V', "...-" }, { 'W', ".--" }, { 'X', "-..-" },
+            { 'Y', "-.--" }, { 'Z', "--.." },
+            { '0', "-----" }, { '1', ".----" }, { '2', "..---" },
+            { '3', "...--" }, { '4', "....-" }, { '5', "....." },
+            { '6', "-...." }, { '7', "--..." }, { '8', "---.." },
+            { '9', "----." }
+        };
+        public static Dictionary<char, string> binaryMorseDictionary = new Dictionary<char, string>
+        {
+            { '0', "-----" },
+            { '1', ".----" }
+        };
+    }
+}
---
+++ TextToMorse/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text;
+using TextToMorse.Commons;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Enter your text here: ");
+                string? input;
+                do
+                {
+                    input = Console.ReadLine();
+                    if (string.IsNullOrEmpty(input))
+                    {
+                        Console.WriteLine("You must input something!");
+                        Console.WriteLine("Enter your text here: ");
+                    }
+                } while (string.IsNullOrEmpty(input));
+
+                (var binaryString, var morseString, var binaryMorseString) = StringConversion(input);
+                Console.WriteLine($"\nBinary: {binaryString}");
+                Console.WriteLine($"Morse: {morseString}");
+                Console.WriteLine($"Binary To Morse: {binaryMorseString}\n");
+            }
+        }
+
+        private static (string binaryString, string morseString, string binaryMorseString) StringConversion(string input)
+        {
+            var binaryString = new StringBuilder();
+            var morseString = new StringBuilder();
+            var binaryMorseString = new StringBuilder();
+
+            foreach (var c in input.ToUpper()) // To uppercase because in morse code it doesn't matter if it's lowercase or uppercase
+            {
+                if (Mapping.morseCodeDictionary.ContainsKey(c))
+                {
+                    morseString.Append(Mapping.morseCodeDictionary[c] + " ");
+
+                    string binary = Convert.ToString(c, 2).PadLeft(8, '0');
+                    binaryString.Append(binary + " ");
+                    foreach (char b in binary)
+                    {
+                        binaryMorseString.Append(Mapping.binaryMorseDictionary[b]);
+                    }
+                    binaryMorseString.Append(" ");
+                }
+
+                else
+                {
+                    return ("Invalid character detected.", string.Empty, string.Empty);
+                }
+            }
+
+            return (binaryString.ToString().Trim(), morseString.ToString().Trim(), binaryMorseString.ToString().Trim());
+        }
+    }
+}
